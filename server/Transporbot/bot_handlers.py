34fabcc927fa6a6c08c0f7bc377ff2a33992c53f
JaviBF92 <javibf1992@gmail.com>
2016-06-14 17:56:36 +0200

    Corrección de bot_handlers

--- server/Transporbot/bot_handlers.py
+++ server/Transporbot/bot_handlers.py
@@ -1,4 +1,6 @@
 from Transporbot.views import StartCommandView, HelpCommandView
+from telegrambot.handlers import command, unknown_command
+
 urlpatterns = [command('start', StartCommandView.as_command_view()),
            command('help', HelpCommandView.as_command_view()),
            #regex(r'(?P<org>\w+)_(?P<dst>\w+)', AuthorName.as_command_view()),
