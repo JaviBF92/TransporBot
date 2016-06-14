41cb220a12aeb8c6057e6fc6ad02a05a8935838e
JaviBF92 <javibf1992@gmail.com>
2016-06-14 18:11:48 +0200

    Direccion estatica añadida a settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -122,4 +122,5 @@ TELEGRAM_BOT_HANDLERS_CONF = "Transporbot.bot_handlers"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/templates/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
