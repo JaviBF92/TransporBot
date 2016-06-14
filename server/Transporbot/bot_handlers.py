06b2f9b9510a64fa66683e46909cab7e63f4333b
JaviBF92 <javibf1992@gmail.com>
2016-06-14 17:23:16 +0200

    Añadidas views y mensajes

--- server/Transporbot/bot_handlers.py
+++ server/Transporbot/bot_handlers.py
@@ -1,4 +1,5 @@
-urlpatterns = [command('start', ),
-           command('help', ),
-           regex(r'(?P<org>\w+)_(?P<dst>\w+)', AuthorName.as_command_view()),
+from Transporbot.views import StartCommandView, HelpCommandView
+urlpatterns = [command('start', StartCommandView.as_command_view()),
+           command('help', HelpCommandView.as_command_view()),
+           #regex(r'(?P<org>\w+)_(?P<dst>\w+)', AuthorName.as_command_view()),
           ]
--- server/Transporbot/views.py
+++ server/Transporbot/views.py
@@ -1,3 +1,9 @@
+telegrambot.bot_views.generic import TemplateCommandView
 from django.shortcuts import render
 
 # Create your views here.
+class StartCommandView(TemplateCommandView):
+    template_text = "templates/messages/command_start_text.txt"
+
+class HelpCommandView(TemplateCommandView):
+    template_text = "templates/messages/command_help_text.txt"
