e26ab4b3ae3fe61a2c70286b5bdbf64e418c4b61
JaviBF92 <javibf1992@gmail.com>
2016-07-19 17:30:05 +0200

    Finalizado tratamiento de destinos con transbordo (ahora se muestra horario de ambos trenes y estación de transbordo); modificación de caché para almacenar transbordos

--- transporbot.py
+++ transporbot.py
@@ -16,16 +16,20 @@ def get_schedule(html):
 
 	if table == None:
 		if any(["No Existen Servicios" in i.text for i in soup.body.div.find_all("h4")]):
-			return []
+			return ("", [], [])
 		else:
-			return None
+			return (None, None, None)
 	else:
 		#Comprueba si es trasbordo
-		if "Origen" in str(table.tbody.contents[3].contents[3]):
+		if any(["Transbordo en" in i.text for i in table.findAll('td', { "class" : "cabe" })]):
+			transbordo = table.tbody.contents[5].td.string.strip()
 			horarios = [i.string.strip() for i in table.findAll('td', { "class" : "color2" })[::3] if i.string != None]
+			horarios_t = [i.string.strip() for i in table.findAll('td', { "class" : "color3" })[::2] if i.string != None]
 		else:
+			transbordo = None
 			horarios = [i.string.strip() for i in table.findAll('td', { "class" : "color1" })[::2] if i.string != None]
-	return horarios
+			horarios_t = []
+	return (transbordo, horarios, horarios_t)
 
 def new_empty_file():
 	call(["touch", "horarios"])
@@ -33,18 +37,15 @@ def new_empty_file():
 	pickle.dump({}, fichero)
 	fichero.close()
 
-def save_schedule(dic, org_dst, fecha, horarios):
-	dic[org_dst] = (fecha, horarios)
+def save_schedule(dic, org_dst, fecha, transbordo, horarios, horarios_t):
+	dic[org_dst] = (fecha, transbordo, horarios, horarios_t)
 	fichero = open('horarios', 'w')
 	pickle.dump(dic, fichero)
 	fichero.close()
 
 
 def return_schedule(entries):
-	hoy = date.today().strftime("%d-%m-%Y")
-
-	if not os.path.isfile("horarios"):
-		new_empty_file()
+	today = date.today().strftime("%Y%m%d")
 
 	fichero = open("horarios", "r")
 	dic = pickle.load(fichero)
@@ -54,15 +55,14 @@ def return_schedule(entries):
 	org_dst = orig + "_" + dest
 	print org_dst
 	horas = []
-	if not org_dst in dic or dic[org_dst][0] != hoy:
-		req_date = date.today().strftime("%Y%m%d")
-		html = get_html(orig, dest, req_date)
-		horas = get_schedule(html)
+	if not org_dst in dic or dic[org_dst][0] != today:
+		html = get_html(orig, dest, today)
+		transbordo, horas, horas_t = get_schedule(html)
 		if horas == None:
 			return "Parece que la web de Renfe no funciona ahora mismo.\nIntentalo mas tarde"
-		save_schedule(dic, org_dst, hoy, horas)
+		save_schedule(dic, org_dst, today, transbordo, horas, horas_t)
 	else:
-		horas = dic[org_dst][1]
+		transbordo, horas, horas_t =  dic[org_dst][1:]
 	#Si al comando se le ha incluido una hora se devuelven las horas a partir de esta
 	#Si no se devuelven las horas a partir de la actual
 	if len(entries) == 3:
@@ -70,14 +70,21 @@ def return_schedule(entries):
 	else:
 		filtro_hora = datetime.now().time()
 	horas = [i for i in horas if datetime.strptime(i, "%H.%M").time() > filtro_hora]
+	horas_t = horas_t[(len(horas_t) - len(horas)):]
 	if not horas:
 		return "Vaya! Parece que no hay trenes disponibles"
 	else:
-		return "\n".join(horas)
+		if transbordo == None:
+			return "\n".join(horas)
+		else:
+			return "Transbordo en: " + transbordo + "\n" + "\n".join([horas[i] + "-" + horas_t[i] for i in range(len(horas))])
 
 def main():
 
 	bot = telebot.TeleBot(token, skip_pending=True)
+
+	if not os.path.isfile("horarios"):
+		new_empty_file()
 
 	stations = get_stations()
 
@@ -94,10 +101,7 @@ def main():
 
 	@bot.message_handler(commands=['estaciones'])
 	def send_stations(message):
-	        reply = ""
-	        for e in sorted(stations.keys()):
-	                reply = reply + e + "\n"
-	        bot.reply_to(message, reply)
+	        bot.reply_to(message, "\n".join(sorted(stations.keys())))
 
 	@bot.message_handler(commands=['sevilla'])
 	def send_schedule(message):
